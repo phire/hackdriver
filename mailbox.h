ec273583b12adcce0c05c17e5ed25ff131e2b96a
Scott Mansell <phiren@gmail.com>
2014-03-04 00:22:41 +0000

    Execute a null control list.

--- mailbox.h
+++ mailbox.h
@@ -50,6 +50,19 @@ unsigned int execute_code(int file_desc, unsigned int code, unsigned int r0, uns
 unsigned int execute_qpu(int file_desc, unsigned int num_qpus, unsigned int control, unsigned int noflush, unsigned int timeout);
 unsigned int qpu_enable(int file_desc, unsigned int enable);
 
+
+// Flags for allocate memory.
+enum {
+MEM_FLAG_DISCARDABLE = 1 << 0, /* can be resized to 0 at any time. Use for cached data */
+MEM_FLAG_NORMAL = 0 << 2, /* normal allocating alias. Don't use from ARM */
+MEM_FLAG_DIRECT = 1 << 2, /* 0xC alias uncached */
+MEM_FLAG_COHERENT = 2 << 2, /* 0x8 alias. Non-allocating in L2 but coherent */
+MEM_FLAG_L1_NONALLOCATING = (MEM_FLAG_DIRECT | MEM_FLAG_COHERENT), /* Allocating in L2 */
+MEM_FLAG_ZERO = 1 << 4,  /* initialise buffer to all zeros */
+MEM_FLAG_NO_INIT = 1 << 5, /* don't initialise (default is initialise to all ones */
+MEM_FLAG_HINT_PERMALOCK = 1 << 6, /* Likely to be locked for long periods of time. */
+};
+
 #ifdef C_PLUS_PLUS
 }
 #endif
--- test.cpp
+++ test.cpp
@@ -3,14 +3,61 @@
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <unistd.h>
+#include <stdint.h>
 
 #include "mailbox.h"
 
 // I/O access
 volatile unsigned *v3d;
+int mbox;
+
+// Execute a nop control list to prove that we have contol.
+void testControlLists() {
+// First we allocate and map some videocore memory to build our control list in.
+
+  // ask the blob to allocate us 256 bytes with 4k alignment, zero it.
+  unsigned int handle = mem_alloc(mbox, 0x100, 0x1000, MEM_FLAG_COHERENT | MEM_FLAG_ZERO);
+  if (!handle) {
+    printf("Error: Unable to allocate memory");
+    return;
+  }
+  // ask the blob to lock our memory at a single location at give is that address.
+  uint32_t bus_addr = mem_lock(mbox, handle); 
+  // map that address into our local address space.
+  uint8_t *list = (uint8_t*) mapmem(bus_addr, 0x100);
+
+// Now we construct our control list.
+  // 255 nops, with a halt somewhere in the middle
+  for(int i = 0; i < 0xff; i++) {
+    list[i] = 1; // NOP
+  }
+  list[0xbb] = 0; // Halt.
+
+// And then we setup the v3d pipeline to execute the control list.
+  printf("V3D_CT0CS: 0x%08x\n", v3d[0x100/4]); 
+  printf("Start Address: 0x%08x\n", bus_addr);
+  // Current Address = start of our list (bus address)
+  v3d[0x110/4] = bus_addr;
+  // End Address = just after the end of our list (bus address) 
+  // This also starts execution.
+  v3d[0x108/4] = bus_addr + 0x100;
+  
+  // print status while running
+  printf("V3D_CT0CS: 0x%08x, Address: 0x%08x\n", v3d[0x100/4], v3d[0x110/4]);
+
+  // Wait a second to be sure the contorl list execution has finished
+  sleep(1);
+  // print the status again.
+  printf("V3D_CT0CS: 0x%08x, Address: 0x%08x\n", v3d[0x100/4], v3d[0x110/4]);
+
+// Release memory;
+  unmapmem((void *) list, 0x100);
+  mem_unlock(mbox, handle);
+  mem_free(mbox, handle);
+}
 
 int main(int argc, char **argv) {
-  int mbox = mbox_open();
+  mbox = mbox_open();
 
   // The blob now has this nice handy call which powers up the v3d pipeline.
   qpu_enable(mbox, 1);
@@ -18,12 +65,13 @@ int main(int argc, char **argv) {
   // map v3d's registers into our address space.
   v3d = (unsigned *) mapmem(0x20c00000, 0x1000);
 
-  if(v3d[0] != 0x02443356) {
+  if(v3d[0] != 0x02443356) { // Magic number.
     printf("Error: V3D pipeline isn't powered up and accessable.\n");
     exit(-1);
   }
 
   // We now have access to the v3d registers, we should do something.
+  testControlLists();
 
   return 0;
 }
