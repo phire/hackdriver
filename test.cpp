c5608d166361f80d7c29b702ce93ca945e298d86
Scott Mansell <phiren@gmail.com>
2014-03-07 12:03:59 +0000

    Test for the binner.

--- test.cpp
+++ test.cpp
@@ -4,6 +4,7 @@
 #include <sys/mman.h>
 #include <unistd.h>
 #include <stdint.h>
+#include <assert.h>
 
 #include "mailbox.h"
 #include "v3d.h"
@@ -47,12 +48,184 @@ void testControlLists() {
   printf("V3D_CT0CS: 0x%08x, Address: 0x%08x\n", v3d[V3D_CT0CS], v3d[V3D_CT0CA]);
 
   // Wait a second to be sure the contorl list execution has finished
-  sleep(1);
+  while(v3d[V3D_CT0CS] & 0x20);
+
   // print the status again.
   printf("V3D_CT0CS: 0x%08x, Address: 0x%08x\n", v3d[V3D_CT0CS], v3d[V3D_CT0CA]);
 
 // Release memory;
   unmapmem((void *) list, 0x100);
+  mem_unlock(mbox, handle);
+  mem_free(mbox, handle);
+}
+
+void addbyte(uint8_t **list, uint8_t d) {
+  *((*list)++) = d;
+}
+
+void addshort(uint8_t **list, uint16_t d) {
+  *((*list)++) = (d) & 0xff;
+  *((*list)++) = (d >> 8)  & 0xff;
+}
+
+void addword(uint8_t **list, uint32_t d) {
+  *((*list)++) = (d) & 0xff;
+  *((*list)++) = (d >> 8)  & 0xff;
+  *((*list)++) = (d >> 16) & 0xff;
+  *((*list)++) = (d >> 24) & 0xff;
+}
+
+void addfloat(uint8_t **list, float f) {
+  uint32_t d = *((uint32_t *)&f);
+  *((*list)++) = (d) & 0xff;
+  *((*list)++) = (d >> 8)  & 0xff;
+  *((*list)++) = (d >> 16) & 0xff;
+  *((*list)++) = (d >> 24) & 0xff;
+}
+
+
+void testBinner() {
+// Like above, we allocate/lock/map some videocore memory
+  // 64kb, 4k alignment
+  unsigned int handle = mem_alloc(mbox, 0x10000, 0x1000, MEM_FLAG_COHERENT | MEM_FLAG_ZERO);
+  if (!handle) {
+    printf("Error: Unable to allocate memory");
+    return;
+  }
+  uint32_t bus_addr = mem_lock(mbox, handle); 
+  uint8_t *list = (uint8_t*) mapmem(bus_addr, 0x10000);
+
+  uint8_t *p = list;
+
+// Configuration stuff
+  // Tile Binning Configuration.
+  //   Tile state data is 48 bytes per tile, I think it can be thrown away
+  //   as soon as binning is finished.
+  addbyte(&p, 112);
+  addword(&p, bus_addr + 0x6200); // tile allocation memory address
+  addword(&p, 0x9c00); // tile allocation memory size
+  addword(&p, bus_addr + 0x100); // Tile state data address
+  addbyte(&p, 30); // 1920/64
+  addbyte(&p, 17); // 1080/64 (16.875)
+  addbyte(&p, 0x04); // config
+
+  // Start tile binning.
+  addbyte(&p, 6);
+
+  // Primitive type
+  addbyte(&p, 56);
+  addbyte(&p, 0x32); // 16 bit triangle
+
+  // Clip Window
+  addbyte(&p, 102);
+  addshort(&p, 0);
+  addshort(&p, 0);
+  addshort(&p, 1920); // width
+  addshort(&p, 1080); // height
+
+  // State
+  addbyte(&p, 96);
+  addbyte(&p, 0x03); // enable both foward and back facing polygons
+  addbyte(&p, 0x00); // depth testing disabled
+  addbyte(&p, 0x02); // enable early depth write
+
+  // Viewport offset
+  addbyte(&p, 103);
+  addshort(&p, 0);
+  addshort(&p, 0);
+
+// The triangle
+  // No Vertex Shader state (takes pre-transformed vertexes, 
+  // so we don't have to supply a working coordinate shader to test the binner.
+  addbyte(&p, 65);
+  addword(&p, bus_addr + 0x80); // Shader Record
+
+  // primitive index list
+  addbyte(&p, 32);
+  addbyte(&p, 0x04); // 8bit index, trinagles
+  addword(&p, 3); // Length
+  addword(&p, bus_addr + 0x70); // address
+  addword(&p, 2); // Maximum index
+
+// End of bin list
+  // Flush
+  addbyte(&p, 5);
+  // Nop
+  addbyte(&p, 1);
+  // Halt
+  addbyte(&p, 0);
+
+  int length = p - list;
+  assert(length < 0x80);
+
+// Shader Record
+  p = list + 0x80;
+  addbyte(&p, 0x01); // flags
+  addbyte(&p, 6*4); // stride
+  addbyte(&p, 0xcc); // num uniforms (not used)
+  addbyte(&p, 3); // num varyings
+  addword(&p, 0xaaaaaaaa); // Fragment shader code
+  addword(&p, 0xbbbbbbbb); // Fragment shader uniforms
+  addword(&p, bus_addr + 0xa0); // Vertex Data
+
+// Vertex Data
+  p = list + 0xa0;
+  // Vertex: Top, red
+  addshort(&p, 200 << 4); // Y in 12.4 fixed point
+  addshort(&p, (1920/2) << 4); // X in 12.4 fixed point
+  addfloat(&p, 1.0f); // Z
+  addfloat(&p, 1.0f); // 1/W
+  addfloat(&p, 1.0f); // Varying 0 (Red)
+  addfloat(&p, 0.0f); // Varying 1 (Green)
+  addfloat(&p, 0.0f); // Varying 2 (Blue)
+
+  // Vertex: bottom left, Green
+  addshort(&p, 800 << 4); // Y in 12.4 fixed point
+  addshort(&p, 560 << 4); // X in 12.4 fixed point
+  addfloat(&p, 1.0f); // Z
+  addfloat(&p, 1.0f); // 1/W
+  addfloat(&p, 0.0f); // Varying 0 (Red)
+  addfloat(&p, 1.0f); // Varying 1 (Green)
+  addfloat(&p, 0.0f); // Varying 2 (Blue)
+
+  // Vertex: bottom right, Blue
+  addshort(&p, 800 << 4); // Y in 12.4 fixed point
+  addshort(&p, 1460 << 4); // X in 12.4 fixed point
+  addfloat(&p, 1.0f); // Z
+  addfloat(&p, 1.0f); // 1/W
+  addfloat(&p, 0.0f); // Varying 0 (Red)
+  addfloat(&p, 0.0f); // Varying 1 (Green)
+  addfloat(&p, 1.0f); // Varying 2 (Blue)
+
+// Vertex list
+  p = list + 0x70;
+  addbyte(&p, 0); // top
+  addbyte(&p, 1); // bottom left
+  addbyte(&p, 2); // bottom right
+
+// Run our control list
+  printf("Binner control list constructed\n");
+  printf("Start Address: 0x%08x, length: 0x%x\n", bus_addr, length);
+
+  v3d[V3D_CT0CA] = bus_addr;
+  v3d[V3D_CT0EA] = bus_addr + length;
+  printf("V3D_CT0CS: 0x%08x, Address: 0x%08x\n", v3d[V3D_CT0CS], v3d[V3D_CT0CA]);
+
+  // Wait for control list to execute
+  while(v3d[V3D_CT0CS] & 0x20);
+  
+  printf("V3D_CT0CS: 0x%08x, Address: 0x%08x\n", v3d[V3D_CT0CS], v3d[V3D_CT0CA]);
+
+  // just dump the buffer to a file
+  FILE *f = fopen("binner_dump.bin", "w");
+  for(int i = 0; i < 0x10000; i++) {
+    fputc(list[i], f);
+  }
+  fclose(f);
+  printf("Buffer containing binned tile lists dumpped to binner_dump.bin\n");
+
+// Release resources
+  unmapmem((void *) list, 0x10000);
   mem_unlock(mbox, handle);
   mem_free(mbox, handle);
 }
@@ -72,7 +245,7 @@ int main(int argc, char **argv) {
   }
 
   // We now have access to the v3d registers, we should do something.
-  testControlLists();
+  testBinner();
 
   return 0;
 }
